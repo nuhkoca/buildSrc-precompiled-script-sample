4c7aad536e3a33578a4bc33ae919211e54c4e960
nuhkoca <nuhkocaa@gmail.com>
2021-06-11 01:30:16 +0200

    Initial commit

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    `java-gradle-plugin`
+    `kotlin-dsl`
+    `kotlin-dsl-precompiled-script-plugins`
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.android.tools.build:gradle:4.2.1")
+    implementation(kotlin("gradle-plugin", version = "1.5.10"))
+}
---
+++ buildSrc/src/main/kotlin/android-defaults.gradle.kts
@@ -0,0 +1,36 @@
+import com.android.build.gradle.BaseExtension
+
+// How do I add another plugin in here? Below ones not working..
+/*plugins {
+    kotlin("android")
+}*/
+
+/*configure<PluginDependenciesSpec> {
+    kotlin("android")
+}*/
+
+configure<BaseExtension> {
+
+    testOptions {
+        @Suppress("UnstableApiUsage")
+        unitTests {
+            isReturnDefaultValues = true
+        }
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+        }
+        maybeCreate("internal").apply {
+            isMinifyEnabled = true
+            isDebuggable = true
+        }
+        getByName("release") {
+            isMinifyEnabled = true
+            isZipAlignEnabled = true
+            isDebuggable = false
+            isJniDebuggable = false
+        }
+    }
+}
